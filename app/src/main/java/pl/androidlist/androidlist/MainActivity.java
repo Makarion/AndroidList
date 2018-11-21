4d83b3f91700c051f183a4764b3ced9e7a06d985
Makarion <kamak96@op.pl>
2018-11-21 19:28:11 +0100

    Adding image from gallery vol 1

--- app/src/main/java/pl/androidlist/androidlist/MainActivity.java
+++ app/src/main/java/pl/androidlist/androidlist/MainActivity.java
@@ -1,13 +1,94 @@
 package pl.androidlist.androidlist;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageActivity;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import static com.theartofdev.edmodo.cropper.CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText mEditDataWyjazdu, mEditDataPowrotu, mEditCena, mEditLokalizacja;
+    Button btnDodaj, btnPokaz;
+    ImageView mObrazek;
+
+    static int REQUEST_CODE_GALLERY=999;
+
+    // nadanie uprawnień użytkownikowi aplikacji do dostępu do galerii
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == REQUEST_CODE_GALLERY){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent,REQUEST_CODE_GALLERY);
+            }
+            else{
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    // wstawienie obrazka z galerii
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(requestCode == REQUEST_CODE_GALLERY && resultCode == RESULT_OK){
+            Uri imageUri=data.getData();
+            CropImage.activity(imageUri).setGuidelines(CropImageView.Guidelines.ON)// guidelines
+            .setAspectRatio(1,1)  //kwadratowy obrazek :)
+            .start(this);
+        }
+        if(requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if(resultCode == RESULT_OK){
+                Uri resultUri = result.getUri();
+                mObrazek.setImageURI(resultUri);
+            }
+            else if (resultCode == CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE){
+                Exception error= result.getError();
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEditDataWyjazdu=findViewById(R.id.dataWyjazdu);
+        mEditDataPowrotu=findViewById(R.id.dataPowrotu);
+        mEditCena=findViewById(R.id.cena);
+        mEditLokalizacja=findViewById(R.id.lokalizacja);
+        mObrazek=findViewById(R.id.obrazek);
+        btnDodaj=findViewById(R.id.dodajWyjazd);
+        btnPokaz=findViewById(R.id.pokazWyjazdy);
+
+        mObrazek.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_CODE_GALLERY
+                );
+            }
+        });
     }
 }
